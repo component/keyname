5f8a38d0f3684a6e682a299b1bc16d477ff18e78
TJ Holowaychuk <tj@vision-media.ca>
2012-10-15 15:45:53 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,49 @@
+
+# keyname
+
+  Keyboard event key name utility
+
+## Installation
+
+    $ component install component/keyname
+
+## API
+
+```js
+var keyname = require('keyname');
+
+keyname(27);
+// => "esc"
+
+keyname(23123123);
+// => undefined
+```
+
+## Keys
+
+  The following are supported:
+
+  - backspace
+  - tab
+  - enter
+  - shift
+  - ctrl
+  - alt
+  - capslock
+  - esc
+  - space
+  - pageup
+  - pagedown
+  - end
+  - home
+  - left
+  - up
+  - right
+  - down
+  - ins
+  - del
+  - meta
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,41 @@
+
+/**
+ * Key name map.
+ */
+
+var map = {
+  8: 'backspace',
+  9: 'tab',
+  13: 'enter',
+  16: 'shift',
+  17: 'ctrl',
+  18: 'alt',
+  20: 'capslock',
+  27: 'esc',
+  32: 'space',
+  33: 'pageup',
+  34: 'pagedown',
+  35: 'end',
+  36: 'home',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  45: 'ins',
+  46: 'del',
+  91: 'meta',
+  93: 'meta',
+  224: 'meta'
+};
+
+/**
+ * Return key name for `n`.
+ *
+ * @param {Number} n
+ * @return {String}
+ * @api public
+ */
+
+module.exports = function(n){
+  return map[n];
+};
